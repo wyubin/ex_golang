25dcdec55850f3815204c35e9a6d2c0b78082241
wyubin <yubin0611@gmail.com>
2021-03-06 04:48:02 +0000

    add factory_method script

---
+++ script/factory_method/interfaceGun.go
@@ -0,0 +1,29 @@
+package main
+
+type iGun interface {
+    setName(name string)
+    setPower(power int)
+    getName() string
+    getPower() int
+}
+
+type gun struct {
+    name  string
+    power int
+}
+
+func (g *gun) setName(name string) {
+    g.name = name
+}
+
+func (g *gun) getName() string {
+    return g.name
+}
+
+func (g *gun) setPower(power int) {
+    g.power = power
+}
+
+func (g *gun) getPower() int {
+    return g.power
+}
---
+++ script/factory_method/structGuns.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type ak47 struct {
+    gun
+}
+
+func (g *ak47) getName() string {
+    return "AK4777777"
+}
+
+func newAk47() iGun {
+    return &ak47{
+        gun: gun{
+            name:  "AK47 gun",
+            power: 4,
+        },
+    }
+}
+
+type musket struct {
+    gun
+}
+
+func (g *musket) getName() string {
+    return "No name"
+}
+
+func newMusket() iGun {
+    return &musket{
+        gun: gun{
+            name:  "Musket gun",
+            power: 1,
+        },
+    }
+}
+
+func getGun(gunType string) (iGun, error) {
+    if gunType == "ak47" {
+        return newAk47(), nil
+    }
+    if gunType == "musket" {
+        return newMusket(), nil
+    }
+    return nil, fmt.Errorf("Wrong gun type passed")
+}
